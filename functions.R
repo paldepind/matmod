ea7af3470aef973928fabce92ee9be16caa42ed6
paldepind <simonfv@gmail.com>
2017-02-15 22:48:57 +0100

    Add Bartletts test for k observation rows

--- functions.R
+++ functions.R
@@ -6,6 +6,18 @@ library(gsubfn)
 interpolate <- fn$identity
 int <- fn$identity
 
+## list <- structure(NA,class="result")
+## "[<-.result" <- function(x,...,value) {
+##    args <- as.list(match.call())
+##    args <- args[-c(1:2,length(args))]
+##    length(value) <- length(args)
+##    for(i in seq(along=args)) {
+##      a <- args[[i]]
+##      if(!missing(a)) eval.parent(substitute(a <- v,list(a=a,v=value[[i]])))
+##    }
+##    x
+## }
+
 # to ease migration from Octave
 printf <- function(...) cat(sprintf(...))
 
@@ -13,22 +25,78 @@ html <- function(...) display_html(paste(...))
 eq <- function(...)
     display_latex(gsub("<-", "\\leftarrow", paste("$", ..., "$"), fixed = TRUE))
 
-calcStuff <- function(n, S, USS) {
+t = function(meanEstimated, meanTry, variance, n) {
+    return((meanEstimated - meanTry) / sqrt(variance / n));
+}
+
+# The likelihood ratio test size
+Q <- function(mean, meanGuess, variance, n) {
+    return(((1 + (t(mean, meanGuess, variance, n)^2)) / (n - 1))^(n / 2));
+}
+
+standardCalculations <- function(obs) {
+    n = length(obs)
+    f = n - 1
+    S = sum(obs)
+    USS = sum (obs ^ 2)
+    SSD = USS - S^2 / n # sum of squares of deviations
+    mean = S / n
+    variance = SSD / (n - 1)
+    return(list(n = n, f = f, S = S, USS = USS, SSD = SSD, mean = mean, variance = variance))
+}
+
+printStandardCalculations <- function(obs) {
+    html("Datasæt: ", repr_html(obs))
+    c = standardCalculations(obs)
+    eq(int("n = `c$n`"))
+    eq(int("S = `c$S`"))
+    eq(int("USS = `c$USS`"))
+    printStuff(c$n, c$S, c$USS)
+}
+
+printStuff <- function(n, S, USS) {
     mean <- S / n;
     SSD <- USS - S^2 / n; # sum of squares of deviations
-    variance <- SSD / (n - 1);
+    variance <- SSD / (n - 1); # usually denoted as s^2
     f <- n - 1; # degrees of freedom
+    StdError <- variance / n;
     sigmaLower <- (f * variance) / qchisq(0.975, f);
     sigmaUpper <- (f * variance) / qchisq(0.025, f);
 
     eq(int("f = n - 1 = `f`"))
     eq(int("SSD = USS - S^2 = `SSD`"))
     html("Estimeret middelværdi")
-    eq(interpolate("\\mu \\leftarrow \\bar{x}. = \\frac{S}{n} = \\frac{`S`}{`n`} = `mean`"))
+    eq(interpolate("\\mu \\leftarrow \\bar{x}. = \\frac{S}{n} = \\frac{`S`}{`n`} = `mean` \\sim\\sim N(\\mu, \\frac{\\sigma^2}{n})"))
+    eq(int("StdError = \\sqrt{s^2 / n} = \\sqrt{`variance` / `n`} = `StdError`"))
     html("95% konfidensinterval for $\\mu$");
-    eq(interpolate("c_{95}(\\mu) = x. \\mp t_{0.975}(f) \\sqrt{(s^2/n)} = `mean` \\mp `qt(0.975, f)`"))
+    eq(interpolate("c_{95}(\\mu) = \\bar{x.} \\mp t_{0.975}(f) StdError = `mean` \\mp `qt(0.975, f)*StdError`"))
     html("Estimeret varians")
     eq(interpolate("\\sigma^2 <- s^2 = \\frac{SSD}{f} = \\frac{`SSD`}{`f`} = `variance`"))
     html("Konfidensinterval for $\\sigma^2$")
     eq(int("c_{95}(\\sigma^2) = [\\frac{f s^2}{\\chi^2_{0.975}(f)}, \\frac{f s^2}{\\chi^2_{0.025}(f)}] = [`sigmaLower`, `sigmaUpper`]"))
+}
+
+calcC <- function(k, fs, f1) {
+    return(1 + 1/(3 * (k - 1)) * (Reduce("+", Map(function(f) 1/f, fs)) - (1/f1)))
+}
+
+# Bartletts test
+calcBa <- function(k, fs, f1, s1, dataList) {
+    denominator = f1 * log(s1) - Reduce("+", Map(function(data) data$f * log(data$variance), dataList))
+    return(denominator / calcC(k, fs, f1))
+}
+
+kObservations <- function(rows) {
+    # number of observations
+    k = length(rows)
+    dataList = Map(standardCalculations, rows)
+    fs = Map(function(data) data$f, dataList)
+    f1 = Reduce("+", fs);
+    ss = Map(function(data) data$variance, dataList)
+    s1 = Reduce("+", Map(function(data) data$SSD, dataList)) / Reduce("+", Map(function(data) data$f, dataList));
+    Ba = calcBa(k, fs, f1, s1, dataList)
+    html("<h2>Tester hypotese om varianshomogenitet</h2>")
+    eq("H_{0\\sigma^2}: \\sigma_1^2 = \\dots = \\sigma_k^2 = \\sigma^2")
+    eq(int("C = ... = `calcC(k, fs, f1)`"))
+    eq(int("Ba = \\frac{-2 ln(Q(x))}{C} = `Ba`"))
 }
