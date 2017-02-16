dc02e53e539cd711608486a0399492cf55148d58
paldepind <simonfv@gmail.com>
2017-02-16 14:28:41 +0100

    Additions to test for common mean

--- functions.R
+++ functions.R
@@ -18,6 +18,8 @@ int <- fn$identity
 ##    x
 ## }
 
+Sum <- function(list) Reduce("+", list)
+
 # to ease migration from Octave
 printf <- function(...) cat(sprintf(...))
 
@@ -87,16 +89,42 @@ calcBa <- function(k, fs, f1, s1, dataList) {
 }
 
 kObservations <- function(rows) {
-    # number of observations
+    ## number of observations
     k = length(rows)
     dataList = Map(standardCalculations, rows)
+    ## display(dataList)
     fs = Map(function(data) data$f, dataList)
     f1 = Reduce("+", fs);
     ss = Map(function(data) data$variance, dataList)
-    s1 = Reduce("+", Map(function(data) data$SSD, dataList)) / Reduce("+", Map(function(data) data$f, dataList));
+    totalSSD = Reduce("+", Map(function(data) data$SSD, dataList));
+    s1 = totalSSD / Sum(Map(function(data) data$f, dataList));
+    totalS = Sum(Map(function(data) data$S, dataList))
+    totaln = Sum(Map(function(data) data$n, dataList))
     Ba = calcBa(k, fs, f1, s1, dataList)
-    html("<h2>Tester hypotese om varianshomogenitet</h2>")
+    pObs = 1 - pchisq(Ba, k - 1)
+    SSD2 = Reduce("+", Map(function(data) data$S^2 / data$n, dataList)) - (totalS^2 / totaln)
+    ## variance2
+    # Print output
+    html(int("Antal observationer: $ k = `k` $"))
+    html("Estimeret varians")
+    eq(int("s_1^2 = `s1`"))
+    eq(int("f_1 = \\sum_{i=1}^{k} f_{(i)} = `f1`"))
+    eq(int("SSD_1 = `totalSSD`"))
+    html("<h2>Test af hypotese om varianshomogenitet</h2>")
     eq("H_{0\\sigma^2}: \\sigma_1^2 = \\dots = \\sigma_k^2 = \\sigma^2")
-    eq(int("C = ... = `calcC(k, fs, f1)`"))
+    eq(int("C = 1 + \\frac{1}{3(k-1)} ((\\sum_{i=1}^{k}\\frac{1}{f_{(i)}}) - \\frac{1}{f_1}) = `calcC(k, fs, f1)`"))
     eq(int("Ba = \\frac{-2 ln(Q(x))}{C} = `Ba`"))
+    eq(int("p_{obs}(x) = 1 - F_{\\chi^2(k-1)}(Ba) = 1 - F_{\\chi^2(`k-1`)}(`Ba`) = `pObs`"))
+    if (pObs > 0.05) {
+        pObs2 = 1 -
+        html("Da $pObs(x)$ er større end $0.05$ kan hypotesen om fælles varians <b>ikke</b> forkastes.")
+        html("<h2>Test af hypotese om ens middelværdi</h2>")
+        eq(int("S. = `totalS`"))
+        eq(int("SSD_2 = (\\sum_{i=1}^{k} \\frac{S_i^2}{n_i}) - \\frac{S.^2}{n.} = `SSD2`"))
+        eq("H_{0\\mu}: \\mu_1 = \\dots = \\mu_k = \\mu")
+        eq(int("s_2^2 = \\frac{SSD_2}{f} = ..."))
+        eq(int("p_{obs}(x) = 1 - F_{F(f, n. - k)} = ..."))
+    } else {
+        html("Da $pObs$ er mindre end $0.05$ <b>forkastes</b> hypotesen om fælles varians.")
+    }
 }
