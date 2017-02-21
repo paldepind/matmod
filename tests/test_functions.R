f65c948e13c845592f21b27c625028c596379983
Jonas Damtoft <Jomik@noreply.users.github.com>
2017-02-21 15:01:15 +0100

    Add 2 tests for linearRegressionEstimates alpha and beta

--- tests/test_functions.R
+++ tests/test_functions.R
@@ -1,3 +1,9 @@
-test_that("one plus one is two", {
-    expect_equal(1 + 1, 2)
+describe("linearRegressionEstimates", {
+    estimates = linearRegressionEstimates(n=13, Sx=2230.7, St = 736, USSx = 3853587.57, USSt = 42702, SPxt = 127872.1)
+    it("can calculate an estimate for beta", {
+        expect_equal(estimates$betaEstimate, 1.53, tolerance = 0.005)
+    })
+    it("can calculate an estimate for alpha", {
+        expect_equal(estimates$alphaEstimate, 84.99, tolerance = 0.005)
+    })
 })
