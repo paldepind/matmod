a0a14feee06ee4ac3679d1963fb50a3017776c5e
Cznielsen <christian@czn.dk>
2017-03-13 14:07:13 +0100

    Fixed bug in TwoObservations and printTwoObservations. Degrees of freedom used in F-test was supposed to be determined by max- and min- variance.

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
-install.packages(c('repr', 'IRdisplay', 'crayon', 'pbdZMQ', 'devtools'),repos = "http://cran.us.r-project.org")
+install.packages(c('stringr', 'repr', 'IRdisplay', 'crayon', 'pbdZMQ', 'devtools'),repos = "http://cran.us.r-project.org")
 devtools::install_github('IRkernel/IRkernel')
 IRkernel::installspec()  # to register the kernel in the current R installation
 install.packages(c('gsubfn', 'testthat'),repos = "http://cran.us.r-project.org")
